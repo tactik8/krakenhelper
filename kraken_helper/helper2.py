e6838dc4a9acc9312f7e9d54409cd57d3dbaff4a
tactik8 <admin@tactik8.com>
2020-09-15 18:18:18 +0000

    go

---
+++ kraken_helper/helper2.py
@@ -0,0 +1,9 @@
+# Test helper function
+
+class Helper:
+    def __init__(self):
+        self.test = 'test'
+  
+    def get(self):
+        print(self.test)
+  
--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-from helper.helper import Helper
+from kraken_helper.helper import Url
 
-test = Helper()
-print(test.get())
+from kraken_helper.schema import Schema
+
+
+
+url = 'https://www.test.com'
+
+u = Url(url)
+
+domain = u.get_domain()
+
+print(domain)
+
+
+s = Schema()
+
+
+print(s.get_test())
+
+
+main_record = s.get_main_record()
+
+
+print(main_record)
+
+
