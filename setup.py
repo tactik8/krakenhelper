86e924493ae84c93ee73bbcf7b3d547c63cffcb5
tactik8 <48621231+tactik8@users.noreply.github.com>
2020-09-15 08:03:47 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='kraken_helper',
+   version='1.0',
+   description='krkane helper module',
+   author='TActik8',
+   author_email='admin@tactik8.com',
+   packages=['kraken_helper'],  #same as name
+   install_requires=['requests'], #external packages as dependencies
+)
