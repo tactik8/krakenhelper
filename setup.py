3340ba947388d0f2c06ba54a35c7f60ecf742665
tactik8 <admin@tactik8.com>
2020-09-16 15:00:14 +0000

    go

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='krakenhelper',
-    version='1.0.1',
+    version='1.0.2',
     description='kraken helper module',
     author='Tactik8',
     author_email='admin@tactik8.com',
     packages=['krakenhelper'],  #same as name
-    install_requires=['requests', 'validators'], #external packages as dependencies
+    install_requires=['requests', 'validators', 'pytz', 'flask'], #external packages as dependencies
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
